b3e5d5e8b4058fc4e96e4f9f4c6f9b830dba9e77
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-03-25 20:05:23 +0000

    display number when it is clicked

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+const buttons = document.querySelectorAll("button");
+const screen = document.querySelector(".screen");
+
+buttons.forEach(e=>e.addEventListener("click", showNumber))
+
+
+function showNumber(clickEvent) {
+    screen.textContent += clickEvent.target.textContent;
+}
+
+
 function operate(number1, operator, number2) {
     switch(operator){
         case "+":
