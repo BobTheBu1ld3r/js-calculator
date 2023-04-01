d06cc7f7590eaef599f30a171987148ed41ec792
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-04-01 14:57:33 +0100

    Set-up basic calculation functionality
    using equals sign calculator can calculate chain of calculations, still
    easily breaks

--- main.js
+++ main.js
@@ -4,8 +4,43 @@ const screen = document.querySelector(".screen");
 buttons.forEach(e=>e.addEventListener("click", showNumber))
 
 
+
+let text = "";
+let operator = "";
+let display = 0;
+let operand0 = 0;
+
 function showNumber(clickEvent) {
-    screen.textContent += clickEvent.target.textContent;
+    const input = clickEvent.target.textContent;
+
+    if("+-*/=".includes(input)) {
+
+        if(!operand0 && input!=="="){
+            console.log("in this part")
+            operand0 = text;
+            operator = input;
+            text="";
+        }
+
+        else{
+            operand1 = text;
+            text = "";
+            operand0 = operate(++operand0, operator, ++operand1);
+            display = operand0;
+            if(input!=="=") operator = input;
+
+
+        }
+
+    }
+    else{
+        text+= clickEvent.target.textContent;
+
+    }
+
+    if(input!=="=")display+= clickEvent.target.textContent;
+    screen.textContent = display;
+    console.log(text);
 }
 
 
