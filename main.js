3f3b93a34ceeeb86faac1b9396a2e08c6f8515c8
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-04-02 22:13:27 +0100

    fix calculator functionality
    only takes two value pairs at a time which is non-ideal

--- main.js
+++ main.js
@@ -7,50 +7,72 @@ window.addEventListener("keypress", handleKeyPress);
 
 function handleButtonClick(clickEvent) {
     const input = clickEvent.target.textContent;
-    showNumber(input);
+    handleInput(input);
 }
 
 function handleKeyPress (keyEvent) {
     const validInputs = Array.from(buttons).map(e=>e.textContent);
     const input = keyEvent.key;
-    if(validInputs.includes(input)) showNumber(input);
+    if(validInputs.includes(input)) handleInput(input);
 }
 
 let text = "";
-let operator = "";
-let display = 0;
+let operator = null;
+let display = "0";
 let operand0 = 0;
+let start = true;
+screen.textContent = display;
+let operand1 = null;
+let isFirstCalc = true;
 
-function showNumber(input) {
 
-    if("+-*/=".includes(input)) {
+function handleInput(input) {
 
-        if(!operand0 && input!=="="){
-            console.log("in this part")
-            operand0 = text;
-            operator = input;
-            text="";
+    if(isNumber(input)){
+        if(start) {
+            display = "";
+            start = false;
+            operand0 = null;
         }
 
-        else{
-            operand1 = text;
-            text = "";
-            operand0 = operate(++operand0, operator, ++operand1);
-            display = operand0;
-            if(input!=="=") operator = input;
+        text+=input;
+        display+=input
+    }
 
+    if(isOperation(input)){
+        if(!operator){
+            if(!start){
+                operand0 = text;
+                operator = input;
+                text="";
+                display+=input;
+                isFirstCalc = false;
+                console.log("it's the first calc");
+            }
 
+            else{
+                operator = input;
+                text="";
+                display+=input;
+                start = false;
+            }
         }
-
     }
-    else{
-        text+= input;
-
+    
+    if(isEquals(input) && !operand1){
+        console.log("yes");
+        operand1 = text;
+        console.log(`operand0=${operand0}, operator=${operator}, operand1=${operand1}`);
+        const result = operate(+operand0, operator, +operand1)
+        operand0 = result;
+        text="";
+        operand1 = null;
+        display= result;
+        start = true;
+        operator = null;
     }
 
-    if(input!=="=") display+= input;
     screen.textContent = display;
-    console.log(text);
 }
 
 
@@ -67,4 +89,14 @@ function operate(number1, operator, number2) {
     }
 }
 
-console.log(operate(10, "/", 5));
+function isOperation(input) {
+    return "+-*/".includes(input);
+}
+
+function isEquals (input) {
+    return input==="=";
+}
+
+function isNumber(input) {
+    return "0123456789".includes(input);
+}
